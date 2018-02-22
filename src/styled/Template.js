c422b556b88a161cb6ca79a096d900e64e6e1dbb
ryanzola <ryanzola@me.com>
2018-02-22 18:59:28 -0500

    implemented basic routing and game board

---
+++ src/styled/Template.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components';
+import { media } from '../utils/media'
+
+export const Header = styled.header`
+  text-align: center;
+  font-size: 2em;
+  font-family: 'Roboto', sans-serif;
+  ${media.handheld`
+    width: 100%;
+  `}
+`
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
+  width: 80%;
+  min-height: 80vh;
+`
+
+export const Main = (props) => {
+  return(
+    <Container>
+      {props.children}
+    </Container>
+  )
+}
